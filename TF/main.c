94a1180e87896419406e3e60c9d325ba8e805ec4
Lipe-Silva <89425527+Lipe-Silva@users.noreply.github.com>
2022-12-09 20:15:34 -0300

    Update main.c

--- TF/main.c
+++ TF/main.c
@@ -16,9 +16,49 @@ typedef struct caixa {
 
 }
 
+void arquivoSaida(char* arg){
+	
+	int tamanho = strlen(arg);
+	char num[4] = "";
+	char arquivo[16] = "saida_";
+	char tipo_arq[4] = ".txt";
+	
+	if (tamanho > 4) { printf("Erro no Parametro"); return 0;}
+	
+	if ( tamanho == 1 ) { strcpy(num,"000"); strcat(num,arg);}
+	else if ( tamanho == 2 ) { strcpy(num,"00"); strcat(num,arg);}
+	else if ( tamanho == 3 ) { strcpy(num,"0"); strcat(num,arg);}
+	else if ( tamanho == 4 ) { strcat(num,arg); }	
+
+	strcat(arquivo,num);
+	strcat(arquivo,tipo_arq);
+	printf("%s",arquivo);
+	return arquivo;
+}
+
+void arquivoEntrada(char* arg){
+	
+	int tamanho = strlen(arg);
+	char num[4] = "";
+	char arquivo[16] = "entrada_";
+	char tipo_arq[4] = ".txt";
+	
+	if (tamanho > 4) { printf("Erro no Parametro"); return 0;}
+	
+	if ( tamanho == 1 ) { strcpy(num,"000"); strcat(num,arg);}
+	else if ( tamanho == 2 ) { strcpy(num,"00"); strcat(num,arg);}
+	else if ( tamanho == 3 ) { strcpy(num,"0"); strcat(num,arg);}
+	else if ( tamanho == 4 ) { strcat(num,arg); }	
+
+	strcat(arquivo,num);
+	strcat(arquivo,tipo_arq);
+	printf("%s",arquivo);
+	return arquivo;
+}
 
 int main(int narg, char* argv[]) {
 	Fila_FIFO *premium, *ouro, *prata, *bronze, *leezo = malloc(sizeof(Fila_FIFO));
 
+	
 	
 }
